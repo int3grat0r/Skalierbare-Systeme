1ca9dda2e744e97abf4adacd685053eec7674966
Vesela Stefanova <vesela.al.stefanova@gmail.com>
2019-05-05 21:24:39 +0200

    fixed date format

--- Hausaufgabe 2_Frameworks/django_project/todos/catalog/forms.py
+++ Hausaufgabe 2_Frameworks/django_project/todos/catalog/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Todo
-
+from todos import settings
 
 class TodoForm(forms.ModelForm):
 	class Meta:
 		model = Todo
 		fields = ('name', 'deadline', 'progress',)
 		name = forms.CharField(max_length=80)
-		deadline = forms.DateField()
+		deadline = forms.DateField(input_formats=settings.DATE_FORMAT)
 		progress = forms.IntegerField()
