cd7c010519f2b263ca50ef16d840af6e8a4617f0
Vesela Stefanova <vesela.al.stefanova@gmail.com>
2019-05-05 13:45:17 +0200

    todo edit and new

--- Hausaufgabe 2_Frameworks/django_project/todos/catalog/models.py
+++ Hausaufgabe 2_Frameworks/django_project/todos/catalog/models.py
@@ -16,4 +16,5 @@ class Todo(models.Model):
 		return self.name
 
 	def get_absolute_url(self):
-		return reverse('page_edit_task', args=[str(self.id)])
+		# todo
+		return reverse('model-detail-view', args=[str(self.id)])
--- Hausaufgabe 2_Frameworks/django_project/todos/catalog/views.py
+++ Hausaufgabe 2_Frameworks/django_project/todos/catalog/views.py
@@ -1,27 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 import datetime
-
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-
 from catalog.models import Todo
 from .forms import TodoForm
 
 def index(request):
-    """View function for home page of site."""
-
     todo_list = Todo.objects.all()
-
     context = {
         'todo_list': todo_list,
     }
-
     return render(request, 'index.html', context=context)
-
 
 def page_new_task(request):
 	return render(request, 'page_new_task.html')
@@ -31,6 +23,19 @@ def page_edit_task(request):
 
 def page_impressum(request):
 	return render(request, 'page_impressum.html')
+
+def new(request):
+	# todo
+
+def edit(request, pk):
+    elem = Todo.objects.get(pk = pk)
+    # todo
+    # load in form
+    # edit
+
+    elem.save()
+    # render???? context?
+    return HttpResponseRedirect('/catalog/')
 
 def delete(request, pk):
    elem = Todo.objects.get(pk = pk)
